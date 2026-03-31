636214d74339fbf3a288f1fe23eb6bb86c391ddd
Vinil Vidushanka <vidushankav@gmail.com>
2026-03-31 13:50:59 +0530

    Final Fix: Correctly linked both submodules with pointers

--- eca-services/booking-service/src/main/java/com/example/booking_service/repository/BookingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.booking_service.repository;
-
-import com.example.booking_service.entity.Booking;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface BookingRepository extends MongoRepository<Booking, String> {
-}
--- eca-services/customer-service/src/main/java/com/example/customer_service/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.customer_service.repository;
-
-import com.example.customer_service.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
